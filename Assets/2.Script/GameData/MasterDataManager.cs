8d01283f7f95cea60de9e3b0503da2c3c4e81978
wjdrbsdl <wjdrbsdl1011@naver.com>
2025-06-13 17:35:41 +0900

    [추가] 단계 데이터 추가

---
+++ Assets/2.Script/GameData/MasterDataManager.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+public class MasterDataManager : MonoBehaviour
+{
+    public static MasterDataManager Instance;
+    private Dictionary<int, StepData> masterStepDataDictionary;
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            MakeMasterData();
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void MakeMasterData()
+    {
+        masterStepDataDictionary = new();
+        string[] lines = File.ReadAllLines("Assets/4.Data/StepData.csv");
+        foreach (string line in lines)
+        {
+            string[] values = line.Split(',');
+            if (values[0][0] == '#')
+            {
+                continue;
+            }
+
+            StepData parseItem = new StepData(values);
+           // Debug.Log($"파싱한데이터 {parseItem.ID} _ {parseItem.PrintText}");
+            masterStepDataDictionary.Add(parseItem.ID, parseItem);
+        }
+    }
+
+    public Dictionary<int, StepData> GetMasterStepDataDic()
+    {
+        return masterStepDataDictionary;
+    }
+
+    public StepData GetMasterStepData(int stepID)
+    {
+        return masterStepDataDictionary[stepID];
+    }
+}
---
+++ Assets/2.Script/GameData/StepData/StepData.cs
@@ -0,0 +1,67 @@
+﻿
+
+using NUnit.Framework.Internal;
+using System;
+using System.Reflection;
+using UnityEngine;
+using static UnityEngine.GraphicsBuffer;
+
+public enum ClearType
+{
+    Click, Collect, Answer
+}
+
+public class StepData
+{
+    public int ID;
+    public string PrintText;
+    public string Hint;
+    public ClearType ClearType;
+    public string SuccessCode;
+
+    public const int INVALID_NUMBER = -1;
+    public int PreStep;
+    public int NextStep;
+    public int FailStep;
+
+    public StepData(string[] parseData)
+    {
+        int pidIdx = 0;
+        int printTextIdx = pidIdx + 1;
+        int hintIdx = printTextIdx + 1;
+        int ClearTypeIdx = hintIdx + 1;
+        int successCodeIdx = ClearTypeIdx + 1;
+        int preStepIdx = successCodeIdx + 1;
+        int nextStepIdx = preStepIdx + 1;
+        int failStepIdx = nextStepIdx + 1;
+
+        ID = int.Parse(parseData[pidIdx]);
+        PrintText = parseData[printTextIdx];
+        Hint = parseData[hintIdx];
+        ClearType = ParseEnum<ClearType>(parseData[ClearTypeIdx]);
+        SuccessCode = parseData[successCodeIdx];
+        PreStep = int.Parse(parseData[preStepIdx]);
+        NextStep = int.Parse(parseData[nextStepIdx]);
+        FailStep = (int.Parse(parseData[failStepIdx]));
+    }
+
+    private T ParseEnum<T>(string inEnumStr) where T : Enum
+    {
+        T parseEnum = (T)Enum.Parse(typeof(T), inEnumStr);
+        return parseEnum;
+    }
+
+    // 복사 생성자
+    public StepData(StepData origin)
+    {
+        ID = origin.ID;
+        PrintText = origin.PrintText;
+        Hint = origin.Hint;
+        ClearType = origin.ClearType;
+        SuccessCode = origin.SuccessCode;
+        PreStep = origin.PreStep;
+        NextStep = origin.NextStep;
+        FailStep = origin.FailStep;
+    }
+}
+
