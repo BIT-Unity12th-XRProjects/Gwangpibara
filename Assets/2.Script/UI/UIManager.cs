41daf41558bf680d87473803a6b9f3ec4185fcc7
wjdrbsdl <wjdrbsdl1011@naver.com>
2025-06-18 09:42:09 +0900

    [Update] 유아이 매니저 간소화 1. 열고 닫는 함수만 남겨두기 2. UI는 하나만 켜져 있으므로 pool 대신 단일 변수로 수정 3. start에서 startUI 켜보기

--- Assets/2.Script/UI/UIManager.cs
+++ Assets/2.Script/UI/UIManager.cs
@@ -7,27 +7,62 @@ public class UIManager : Singleton<UIManager>
     public Transform UICanvasTrs; //UI화면을 랜더링할 컨버스
     public Transform CloseUITrs; //닫을 때 비활성화 할 
 
-    private BaseUI m_FrontUI; //UI가 열렸을 때 가장 상단에 열려있는 UI
-
     //열려있는, 닫혀있는 ui pool 나눠서 관리
-    private Dictionary<Type, GameObject> m_OpenUIPool = new Dictionary<Type, GameObject>();
-    private Dictionary<Type, GameObject> m_ClosedUIPool = new Dictionary<Type, GameObject>();
+    private BaseUI _openUI;
+    private Dictionary<Type, GameObject> _closedUIPool = new Dictionary<Type, GameObject>();
+
+    private void Start()
+    {
+        OpenUI<StartUI>(new StartUIData());
+    }
+
+    //실제로 여는 함수
+    public void OpenUI<T>(BaseUIData uiData)
+    {
+        Type uiType = typeof(T);
+        bool isAlreadyOpen = false;
+        var ui = GetUI<T>(out isAlreadyOpen);
+
+        if (isAlreadyOpen == true)
+        {
+            return;
+        }
+
+        if (ui == null)
+        {
+            //Logger.LogError($"{uiType} prefab doesn't exist in Resources");
+            return;
+        }
+
+        var siblingIndex = UICanvasTrs.childCount;
+        ui.Init(UICanvasTrs);
+        ui.transform.SetSiblingIndex(siblingIndex);
+        ui.gameObject.SetActive(true);
+        ui.SetInfo(uiData);
+        ui.ShowUI();
+
+        if(_openUI != null)
+        {
+            CloseUI(_openUI);
+        }
+
+        _openUI = ui;
+    }
 
     private BaseUI GetUI<T>(out bool isAlreadyOpen)
     {
         Type uiType = typeof(T);
         BaseUI ui = null;
         isAlreadyOpen = false;
-
-        if (m_OpenUIPool.ContainsKey(uiType) == true)
+        
+        if(_openUI != null && _openUI.GetType() == uiType)
         {
-            ui = m_OpenUIPool[uiType].GetComponent<BaseUI>();
             isAlreadyOpen = true;
         }
-        else if (m_ClosedUIPool.ContainsKey(uiType) == true)
+        else if (_closedUIPool.ContainsKey(uiType) == true)
         {
-            ui = m_ClosedUIPool[uiType].GetComponent<BaseUI>();
-            m_ClosedUIPool.Remove(uiType);
+            ui = _closedUIPool[uiType].GetComponent<BaseUI>();
+            _closedUIPool.Remove(uiType);
         }
         else
         {
@@ -40,88 +75,14 @@ public class UIManager : Singleton<UIManager>
         return ui;
     }
 
-    //실제로 여는 함수
-    public void OpenUI<T>(BaseUIData uiData)
-    {
-        Type uiType = typeof(T);
-        bool isAlreadyOpen = false;
-        var ui = GetUI<T>(out isAlreadyOpen);
-
-        if (ui == null)
-        {
-            //Logger.LogError($"{uiType} prefab doesn't exist in Resources");
-            return;
-        }
-
-        if (isAlreadyOpen == true)
-        {
-            return;
-        }
-
-        var siblingIndex = UICanvasTrs.childCount;
-        ui.Init(UICanvasTrs);
-        ui.transform.SetSiblingIndex(siblingIndex);
-        ui.gameObject.SetActive(true);
-        ui.SetInfo(uiData);
-        ui.ShowUI();
-
-        m_FrontUI = ui;
-        m_OpenUIPool.Add(uiType, ui.gameObject);
-    }
-
     public void CloseUI(BaseUI ui)
     {
         if (ui == null) return;
         Type uiType = ui.GetType();
 
         ui.gameObject.SetActive(false);
-        m_OpenUIPool.Remove(uiType);
-        m_ClosedUIPool[uiType] = ui.gameObject;
+          _closedUIPool[uiType] = ui.gameObject;
         ui.transform.SetParent(CloseUITrs);
-
-        m_FrontUI = null;
-        var lastChild = UICanvasTrs.GetChild(UICanvasTrs.childCount - 1);
-        if (lastChild != null)
-        {
-            m_FrontUI = lastChild.gameObject.GetComponent<BaseUI>();
-        }
     }
 
-    //열려있는 UI 체크해서 가져오는 메소드
-    public BaseUI GetActiveUI<T>()
-    {
-        var uiType = typeof(T);
-        return m_OpenUIPool.ContainsKey(uiType) ? m_OpenUIPool[uiType].GetComponent<BaseUI>() : null;
-    }
-
-    public bool ExistOpenUI() // 열린 게 하나라도 있는지
-    {
-        return m_FrontUI != null;
-    }
-
-    public BaseUI GetCurrentFrontUI() //가장 최상단 ui instance 반환
-    {
-        return m_FrontUI;
-    }
-
-    public void CloseCurrentFrontUI()
-    {
-        if (m_FrontUI != null) m_FrontUI.CloseUI();
-    }
-
-    public void CloseAllOpenUI() //열린 거 다 닫기
-    {
-        while (m_FrontUI != null)
-        {
-            m_FrontUI.CloseUI(true);
-        }
-    }
-    
-    public void CloseTargetUI<T>()
-    {
-        if (GetActiveUI<T>() != null)
-        {
-            CloseUI(GetActiveUI<T>());
-        }
-    }
 }
