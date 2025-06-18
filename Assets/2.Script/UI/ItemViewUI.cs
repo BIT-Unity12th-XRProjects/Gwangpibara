d48a8fe52b2568a7a8ef85314c3d3945f3734dfa
wjdrbsdl <wjdrbsdl1011@naver.com>
2025-06-18 11:27:07 +0900

    [Update] 나가기 버튼 object 연결

--- Assets/2.Script/UI/ItemViewUI.cs
+++ Assets/2.Script/UI/ItemViewUI.cs
@@ -5,18 +5,20 @@ using UnityEngine.UI;
 
 public class ItemViewUI : BaseUI
 {
-    public Button ExitButton = null;
-
     private ItemViewData _viewItemData;
-    private Button m_onExitBtnClicked = null;
+    [SerializeField] private Button _exitButton = null;
     [SerializeField] private ItemViewer _itemViewer;
+
+    private void Awake()
+    {
+        _exitButton.onClick.AddListener(OnClickedExitButton);
+    }
 
     public override void SetInfo(BaseUIData uiData)
     {
         base.SetInfo(uiData);
 
         _viewItemData = uiData as ItemViewData;
-        m_onExitBtnClicked.onClick.AddListener(OnClickedExitButton);
         _itemViewer.ViewItem(_viewItemData);
     }
 
