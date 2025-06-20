9d17b89c97b9dcfba0cbb9e8795eb4a6ea45f95e
doncici77 <pkh2956@naver.com>
2025-06-20 10:10:16 +0900

    [Feature] 생성된 마커 오브젝트의 데이터 값 초기화
    1. 마커 오브젝트 생성하고 ARMarkerObject 컴포넌트 추가
    2. 마커 오브젝트의 GameMarkerData 값을 초기화
    3. _initialized을 이용해서 GameMarkerData 초기화 상태 체크

--- Assets/2.Script/ARPlay/ARMarkerObject.cs
+++ Assets/2.Script/ARPlay/ARMarkerObject.cs
@@ -1,7 +1,23 @@
+using System;
 using UnityEngine;
 
 public class ARMarkerObject : MonoBehaviour
 {
+    private GameMarkerData _markerData;
+
+    // 초기화를 단속하기 위한 변수
+    private bool _initialized = false;
+
+    void Start()
+    {
+        if (!_initialized)
+        {
+            Debug.LogError($"[{name}] ARMarkerObject 가 초기화되지 않았습니다. 반드시 Setting(marker) 를 호출하세요.", this);
+        }
+
+        Debug.Log($"_markId : {_markerData.markId}, _markerType : {_markerData.markerSapwnType}");
+    }
+
     public void TakeRayHit()
     {
         Debug.Log("Camera Hit");
@@ -10,5 +26,13 @@ public class ARMarkerObject : MonoBehaviour
     public void TakeClick()
     {
         Debug.Log("Click Obj");
+    }
+
+    public void Setting(GameMarkerData markerData)
+    {
+        _markerData = markerData;
+        _markerData.markerGameObject = gameObject;
+
+        _initialized = true;
     }
 }
