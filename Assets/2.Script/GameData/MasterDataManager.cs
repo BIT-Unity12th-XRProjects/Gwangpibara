cc879797b84a59241842b8583df174c27c8ed9b5
wjdrbsdl <wjdrbsdl1011@naver.com>
2025-06-20 14:19:21 +0900

    [Feature] 읽을 파일이 없으면 기본 테스트 맵 생성

--- Assets/2.Script/GameData/MasterDataManager.cs
+++ Assets/2.Script/GameData/MasterDataManager.cs
@@ -88,11 +88,17 @@ public class MasterDataManager : MonoBehaviour
         _masterMapDataDictionary = new();
 
         SaveMarkerData loadMarkerData = new();
+
+
         List<MarkerData> loadData = loadMarkerData.LoadMarkerList();
 
         //임시로 1단계 맵 데이터 만들어넣기
-        MapData mapData = new(loadData);
-        _masterMapDataDictionary.Add(1, mapData);
+        MapData map = new MapData();
+        if(loadData.Count == 0)
+        {
+            map = new(loadData);
+        }
+        _masterMapDataDictionary.Add(1, map);
     }
 
   
