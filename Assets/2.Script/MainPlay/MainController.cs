23213361ed688dc70c4e242b6605253a623965ff
wjdrbsdl <wjdrbsdl1011@naver.com>
2025-06-20 11:40:04 +0900

    [Update] 메인컨트롤러 시작로직 수정 1. StartGame()에서 변수들 초기화 2. 진행단계 변수 추가 3. SetStep  - gameUI용 data 생성, 이벤트 호출  - 진행단계 갱신 4. 이전, 다음 단계 이동

--- Assets/2.Script/MainPlay/MainController.cs
+++ Assets/2.Script/MainPlay/MainController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
@@ -7,63 +8,51 @@ public class MainController : MonoBehaviour
     /// <summary>
     /// 게임 진행의 메인 화면
     /// </summary>
-    private StepData _curStepData;
-    [SerializeField] private ItemInventory _itemInventory;
 
-    void Start()
-    {
-        _itemInventory = new();
-    }
+    private int _progressStep; //최대 진행한 단계
+    private StepData _curStepData; //현재 세팅된 단계
+    private GameUIData _gameUIData;
+    [SerializeField] private ItemInventory _itemInventory;
+    public Action<GameUIData> onChangeStepData;
 
     private void Update()
     {
         //테스트 입력 부분 - 추후 UI, 다른 클래스로부터 호출 될 녀석들
         if (Keyboard.current.f5Key.wasPressedThisFrame)
         {
-            ClickNextButton(); //다음 단계 버튼 누르기
-            SubmitAnswer("박광호"); //정답 텍스트 입력하기
+                 SubmitAnswer("박광호"); //정답 텍스트 입력하기
             AcquireItem(MasterDataManager.Instance.GetMasterItemData(10101)); //10101 아이템 전달받기
         }
     }
 
-    public void StartStep(int stepValue)
+    //게임 시작
+    public void StartGame(int stepID)
     {
-        SetStep(MasterDataManager.Instance.GetMasterStepData(stepValue));
+        _itemInventory = new();
+        _gameUIData = new();
+        SetStep(stepID);
+        UIManager.Instance.OpenUI<GameUI>(_gameUIData);
     }
 
-    private void GoNextStep()
+    public void ClickNextButton()
     {
-        int nextStepId = _curStepData.NextStep;
-        if (nextStepId == StepData.INVALID_NUMBER)
+        //이전 단계를 보다가 다음으로 가려는 경우
+        if (_curStepData.ID < _progressStep)
         {
-            Debug.Log("게임 종료");
+            GoNextStep();
             return;
         }
-        StepData nextStep = MasterDataManager.Instance.GetMasterStepData(nextStepId);
-        SetStep(nextStep);
 
-    }
-
-    private void SetStep(StepData stepData)
-    {
-        //현재 단계를 stepData로 맞추기
-        _curStepData = stepData;
-        Debug.Log(stepData.PrintText); //디버그용
-
-        //세팅된 단계의 클리어 조건이 아이템 수집인경우, 이미 수집했는지 체크
-        if(stepData.ClearType == ClearType.Collect)
-        {
-            CheckInventoryCondition();
-        }
-        
-    }
-
-    private void ClickNextButton()
-    {
+        //진행 단계에서 다음으로 가려는 경우
         if (_curStepData.ClearType.Equals(ClearType.Click))
         {
             GoNextStep();
         }
+    }
+
+    public void ClickBeforeButton()
+    {
+        GoBeforeStep();
     }
 
     public void SubmitAnswer(string answer)
@@ -85,12 +74,12 @@ public class MainController : MonoBehaviour
         if (_itemInventory.CheckAcquireItemCondition(itemData, _curStepData.ID) == false)
         {
             //습득불가능한 아이템
-           // Debug.Log("습득 불가");
+            // Debug.Log("습득 불가");
             return false;
         }
         //습득가능하면
         _itemInventory.AddItem(itemData);
-       // Debug.Log($"{itemData.Name} 습득");
+        // Debug.Log($"{itemData.Name} 습득");
         if (_curStepData.ClearType == ClearType.Collect)
         {
             bool isCorrect = CheckAnswer(itemData.ID.ToString());//수집한 단서 ID를 습득했다고 전달 
@@ -103,12 +92,70 @@ public class MainController : MonoBehaviour
         return true;
     }
 
+
+
+    private void SetStep(int stepID)
+    {
+        //현재 단계를 stepData로 맞추기
+        _curStepData = MasterDataManager.Instance.GetMasterStepData(stepID);
+        Debug.Log(_curStepData.PrintText); //디버그용
+
+        _gameUIData.SetData(_curStepData);
+        onChangeStepData?.Invoke(_gameUIData);
+
+        RenewProgress();
+    }
+
+    private void RenewProgress()
+    {
+        //현재 단계와 최종 진행된 단계를 비교해서 진행 수치 반영
+
+        //새로 세팅된 단계가 진행단계보다 낮으면 갱신할 것 없음.
+        if (_curStepData.ID <= _progressStep)
+        {
+            return;
+        }
+
+        //단계가 진행되었다면
+        _progressStep = _curStepData.ID; //진행 수치 조정
+
+        //새로 세팅 된 단계에서 클리어 조건 체크
+        if (_curStepData.ClearType == ClearType.Collect)
+        {
+            CheckInventoryCondition();
+        }
+    }
+
+    private void GoNextStep()
+    {
+        int nextStepId = _curStepData.NextStep;
+        if (nextStepId == StepData.INVALID_NUMBER)
+        {
+            Debug.Log("게임 종료");
+            return;
+        }
+
+        SetStep(nextStepId);
+
+    }
+
+    private void GoBeforeStep()
+    {
+        int beforeStepId = _curStepData.PreStep;
+        if (beforeStepId == StepData.INVALID_NUMBER)
+        {
+            Debug.Log("없는 단계");
+            return;
+        }
+        SetStep(beforeStepId);
+    }
+
     private void CheckInventoryCondition()
     {
         //단계가 바뀌었을 때 현재 단계의 클리어 조건이 아이템 보유고, 이미 해당 아이템을 갖고 있는지 체크
         int needItemID = int.Parse(_curStepData.SuccessCode);
         int haveAmount = _itemInventory.GetItemAmount(needItemID);
-        if(haveAmount >= 1)
+        if (haveAmount >= 1)
         {
             Debug.Log("이미 보유한 아이템 조건 다음 스텝으로");
             GoNextStep();
