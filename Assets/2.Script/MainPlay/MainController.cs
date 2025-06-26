f45d2743c85346246f0b9038ede64eada89dcc88
wjdrbsdl <wjdrbsdl1011@naver.com>
2025-06-27 08:55:51 +0900

    [Feature] 틀린 팝업

--- Assets/2.Script/MainPlay/MainController.cs
+++ Assets/2.Script/MainPlay/MainController.cs
@@ -69,6 +69,7 @@ public class MainController : Singleton<MainController>
             else
             {
                 FeedbackManager.Instance.PlayEffect(false, SFXType.InCorrect);
+                PopUpManager.Instance.PopMessege("틀렸쥬");
             }
         }
     }
